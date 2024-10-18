9a847ee3491495a381f7771dd671d6826105eb50
vishnugitit <vishnujangam1527@gmail.com>
2024-10-18 21:21:59 +0530

    impurejs

--- impure.js
+++ impure.js
@@ -1,7 +1,7 @@
-// content = document.getElementsByTagName("h1");
-//       function impure(a, b) {
-//         res = Math.floor(Math.random() * 10);
-//         solution = a + b + res+" Referesh the page";
-//         return (content[0].innerHTML = solution);
-//       }
-//       console.log(impure(2, 3));
+content = document.getElementsByTagName("h1");
+      function impure(a, b) {
+        res = Math.floor(Math.random() * 10);
+        solution = a + b + res+" Referesh the page";
+        return (content[0].innerHTML = solution);
+      }
+      console.log(impure(2, 3));
