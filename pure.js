1c301a9210a4e9dc52d98a8708a045d5ebc61c28
vishnugitit <vishnujangam1527@gmail.com>
2024-10-18 21:20:49 +0530

    purejs

--- pure.js
+++ pure.js
@@ -1,8 +1,8 @@
-// content = document.getElementsByTagName("h1");
-// function pure(a, b) {
-//   alert(a + b);
-//   return (content[0].innerHTML = a + b);
-// }
-// value_1 = +prompt("Value 1");
-// value_2 = +prompt("Value 2");
-// console.log(pure(value_1, value_2));
+content = document.getElementsByTagName("h1");
+function pure(a, b) {
+  alert(a + b);
+  return (content[0].innerHTML = a + b);
+}
+value_1 = +prompt("Value 1");
+value_2 = +prompt("Value 2");
+console.log(pure(value_1, value_2));
